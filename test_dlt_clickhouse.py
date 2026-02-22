d35d459b3dc774eaa0fae334657d066f9b27df16
jwang <jwang@scaleway.com>
2026-02-22 22:48:50 +0100

    typo

--- test_dlt_clickhouse.py
+++ test_dlt_clickhouse.py
@@ -2,10 +2,9 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from datetime import datetime
 
-
 with DAG(
     dag_id="test_dlt_connection",
-    schedule="@daily",           # <--- Change juste le nom ici
+    schedule="@daily",
     start_date=datetime(2024, 1, 1),
     catchup=False
 ) as dag:
@@ -17,19 +16,23 @@ with DAG(
         image="python:3.11-slim",
         cmds=["bash", "-cx"],
         arguments=[
-            "pip install dlt[clickhouse] && python -c '"
+            # 1. On installe les dépendances
+            # 2. On transforme la variable Airflow en variable DLT au runtime
+            "pip install dlt[clickhouse] && "
+            "export DESTINATION__CLICKHOUSE__CREDENTIALS=$AIRFLOW_CONN_CLICKHOUSE_DEFAULT && "
+            "python -c '"
             "import dlt; "
-            "data = [{\"status\": \"works\", \"env\": \"k8s\", \"timestamp\": \"2026-02-20\"}]; "
+            "import os; "
+            "data = [{\"status\": \"works\", \"env\": \"k8s\", \"timestamp\": \"2026-02-22\"}]; "
             "pipeline = dlt.pipeline(pipeline_name=\"check_scw\", destination=\"clickhouse\", dataset_name=\"test_dlt\"); "
             "info = pipeline.run(data, table_name=\"connection_check\"); "
             "print(info)'"
         ],
-        # On injecte le secret ClickHouse spécifiquement dans ce Pod
         env_from=[{
             "secretRef": {
                 "name": "dwh-connections-secret"
             }
         }],
         get_logs=True,
-        is_delete_operator_pod=False # On le garde pour voir les logs en cas d'erreur
+        is_delete_operator_pod=False 
     )
