1e9d808876368b7fce4859b482abf98e6c52c32e
jwang <jwang@scaleway.com>
2026-02-23 13:55:56 +0100

    add dags for postgre

--- dag_init_postgres.py
+++ dag_init_postgres.py
@@ -3,34 +3,35 @@ from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperato
 from kubernetes.client import models as k8s
 from datetime import datetime
 
+# Ce bloc est INDISPENSABLE pour qu'Airflow voit le DAG
 with DAG(
     dag_id='1_seed_postgres_source',
     start_date=datetime(2026, 1, 1),
     schedule=None,
-    catchup=False
+    catchup=False,
+    tags=['setup']
 ) as dag:
 
-    # On injecte SPÉCIFIQUEMENT le nouveau secret
+    # On appelle le secret créé via Terraform
     env_from_postgres = [
         k8s.V1EnvFromSource(
             secret_ref=k8s.V1SecretEnvSource(name='postgres-connection-secret')
         )
     ]
 
-    seed_db = KubernetesPodOperator(
+    setup_tables = KubernetesPodOperator(
         task_id='create_and_populate_ecommerce',
-        name='init-postgres-pod',
+        name='init-db-pod',
         namespace='airflow',
-        image='postgres:15', # Image légère contenant 'psql'
+        image='postgres:15',
         env_from=env_from_postgres,
         cmds=["bash", "-cx"],
         arguments=[
-            # On utilise la variable d'env injectée par le secret
             'psql "$AIRFLOW_CONN_POSTGRES_SOURCE" -c "'
             'CREATE SCHEMA IF NOT EXISTS ecommerce; '
-            'CREATE TABLE IF NOT EXISTS ecommerce.users (id SERIAL PRIMARY KEY, name TEXT, email TEXT); '
-            'INSERT INTO ecommerce.users (name, email) VALUES (\'Alice\', \'alice@example.com\'), (\'Bob\', \'bob@example.com\');"'
+            'CREATE TABLE IF NOT EXISTS ecommerce.users (id SERIAL PRIMARY KEY, name TEXT); '
+            'INSERT INTO ecommerce.users (name) VALUES (\'Alice\'), (\'Bob\');"'
         ],
-        is_delete_operator_pod=True, # Le pod sera supprimé, mais on aura eu le temps de voir les logs
+        is_delete_operator_pod=True,
         get_logs=True
     )
